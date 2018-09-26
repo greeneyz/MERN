e7d23230fe3ba941d54b3a97684f2fef2a005b42
Asim Imtiaz <asim.imtiaz.ext@hmdglobal.com>
2018-09-26 14:33:41 +0300

    initial commit: Nodejs and express backend with routes defined for users, profile, posts

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    connectionstring: 'mongodb://admin:P%40ssword1@ds115353.mlab.com:15353/devprofile'
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//import API files
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//Body Parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Getting connection string value from config/keys.js file
+const db = require('./config/keys').connectionstring;
+
+//Connecting with MongoDB
+mongoose.connect(db)
+        .then(()=>console.log('MongoDB connected successfully!'))
+        .catch(err=>console.log(err));
+
+app.get ('/', (req, res) => {
+    res.send ('Hello World!')
+});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen (port, () => console.log (`Server running on port ${port}`))
